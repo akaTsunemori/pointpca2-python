feeb8cf87c9c0a3d98c7f79f3b2733ddf1742d1b
akaTsunemori <arthur_07q@hotmail.com>
2024-02-19 17:36:19 -0300

    regression.py - add cli params + polishing

--- regression.py
+++ regression.py
@@ -1,8 +1,9 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from argparse import ArgumentParser
 from lazypredict.Supervised import LazyRegressor
 from scipy.stats import spearmanr, pearsonr
 from sklearn.model_selection import GroupKFold
+# from sklearn.model_selection import train_test_split
 
 
 FEATURES = [f'FEATURE_{i+1}' for i in range(40)]
@@ -47,54 +48,74 @@ def one_group_out_regression(df: pd.DataFrame):
     return result_df
 
 
-def group_k_fold_regression(df_true: pd.DataFrame, df_pred: pd.DataFrame):
-    X_true = df_true[FEATURES]
-    y_true = df_true['SCORE']
-    X_pred = df_pred[FEATURES]
-    y_pred = df_pred['SCORE']
-    groups = df_true['REF']
+def group_k_fold_regression(df_MATLAB: pd.DataFrame, df_Python: pd.DataFrame):
+    X_MATLAB = df_MATLAB[FEATURES]
+    y_MATLAB = df_MATLAB['SCORE']
+    X_Python = df_Python[FEATURES]
+    y_Python = df_Python['SCORE']
+    groups = df_MATLAB['REF']
     gkf = GroupKFold(n_splits=len(groups.unique())) # Adjust n_splits accordingly
-    dataframes_true = list()
-    dataframes_pred = list()
+    dataframes_MATLAB = list()
+    dataframes_Python = list()
     fold = 0
-    for train_idx, test_idx in gkf.split(X_true, y_true, groups):
-        X_train, X_test = X_true.iloc[train_idx], X_true.iloc[test_idx]
-        y_train, y_test = y_true.iloc[train_idx], y_true.iloc[test_idx]
+    for train_idx, test_idx in gkf.split(X_MATLAB, y_MATLAB, groups):
+        X_train, X_test = X_MATLAB.iloc[train_idx], X_MATLAB.iloc[test_idx]
+        y_train, y_test = y_MATLAB.iloc[train_idx], y_MATLAB.iloc[test_idx]
         testing_groups = ', '.join(i for i in groups.iloc[test_idx].unique())
         models = compute_regression(X_train, X_test, y_train, y_test)
         models['Fold'] = fold
         models['Testing Groups'] = testing_groups
-        dataframes_true.append(models)
-        X_train, X_test = X_pred.iloc[train_idx], X_pred.iloc[test_idx]
-        y_train, y_test = y_pred.iloc[train_idx], y_pred.iloc[test_idx]
+        dataframes_MATLAB.append(models)
+        X_train, X_test = X_Python.iloc[train_idx], X_Python.iloc[test_idx]
+        y_train, y_test = y_Python.iloc[train_idx], y_Python.iloc[test_idx]
         models = compute_regression(X_train, X_test, y_train, y_test)
         models['Fold'] = fold
         models['Testing Groups'] = testing_groups
-        dataframes_pred.append(models)
+        dataframes_Python.append(models)
         fold += 1
-    result_df_true = pd.concat(dataframes_true)
-    result_df_true = result_df_true.reset_index(drop=False)
-    result_df_pred = pd.concat(dataframes_pred)
-    result_df_pred = result_df_pred.reset_index(drop=False)
+    result_df_MATLAB = pd.concat(dataframes_MATLAB)
+    result_df_MATLAB = result_df_MATLAB.reset_index(drop=False)
+    result_df_Python = pd.concat(dataframes_Python)
+    result_df_Python = result_df_Python.reset_index(drop=False)
     cols_to_move = ['Testing Groups', 'Fold']
     for col in cols_to_move:
-        column_to_move = result_df_true.pop(col)
-        result_df_true.insert(0, col, column_to_move)
-        column_to_move = result_df_pred.pop(col)
-        result_df_pred.insert(0, col, column_to_move)
-    return result_df_true, result_df_pred
+        column_to_move = result_df_MATLAB.pop(col)
+        result_df_MATLAB.insert(0, col, column_to_move)
+        column_to_move = result_df_Python.pop(col)
+        result_df_Python.insert(0, col, column_to_move)
+    return result_df_MATLAB, result_df_Python
 
-df_pred = pd.read_csv('apsipa_pointpca2_pred_cleaned.csv', index_col=0)
-df_true = pd.read_csv('apsipa_pointpca2_true_cleaned.csv', index_col=0)
-# When using train_test_split (function args need to be adapted)
-# true = compute_regression(df_pred[FEATURES].values, df_pred['SCORE'].values, 'regression_true.csv')
-# pred = compute_regression(df_true[FEATURES].values, df_true['SCORE'].values, 'regression_pred.csv')
-# true.to_csv('regression_true.csv')
-# pred.to_csv('regression_pred.csv')
-true = one_group_out_regression(df_true)
-pred = one_group_out_regression(df_pred)
-true.to_csv('regression_true_LeaveOneGroupOut.csv')
-pred.to_csv('regression_pred_LeaveOneGroupOut.csv')
-true, pred = group_k_fold_regression(df_true, df_pred)
-true.to_csv('regression_true_GroupKFold.csv')
-pred.to_csv('regression_pred_GroupKFold.csv')
+
+def get_args():
+    parser = ArgumentParser()
+    parser.add_argument('--csv_path_MATLAB', type=str,
+                        help='./results/apsipa_pointpca2_MATLAB_cleaned.csv')
+    parser.add_argument('--csv_path_Python', type=str,
+                        help='./results/apsipa_pointpca2_Python_cleaned.csv')
+    parser.add_argument('--dataset_name', type=str,
+                        help='Example: APSIPA')
+    return parser.parse_args()
+
+
+def main(args):
+    csv_path_Python = args.csv_path_Python
+    csv_path_MATLAB = args.csv_path_MATLAB
+    dataset_name = args.dataset_name
+    df_Python = pd.read_csv(csv_path_Python, index_col=0)
+    df_MATLAB = pd.read_csv(csv_path_MATLAB, index_col=0)
+    # When using train_test_split (function args need to be adapted)
+    # MATLAB = compute_regression(df_Python[FEATURES].values, df_Python['SCORE'].values, 'regression_MATLAB.csv')
+    # Python = compute_regression(df_MATLAB[FEATURES].values, df_MATLAB['SCORE'].values, 'regression_Python.csv')
+    # MATLAB.to_csv('regression_MATLAB.csv')
+    # Python.to_csv('regression_Python.csv')
+    MATLAB = one_group_out_regression(df_MATLAB)
+    Python = one_group_out_regression(df_Python)
+    MATLAB.to_csv(f'regression_{dataset_name}_MATLAB_LeaveOneGroupOut.csv')
+    Python.to_csv(f'regression_{dataset_name}_Python_LeaveOneGroupOut.csv')
+    MATLAB, Python = group_k_fold_regression(df_MATLAB, df_Python)
+    MATLAB.to_csv(f'regression_{dataset_name}_MATLAB_GroupKFold.csv')
+    Python.to_csv(f'regression_{dataset_name}_Python_GroupKFold.csv')
+
+
+if __name__ == '__main__':
+    main(get_args())
